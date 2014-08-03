470d4f38e3bae0999b17c519067edfebf0208936
Pavel Smagin <kotapaulta@mail.ru>
2014-08-03 15:32:12 +0600

    add number validation, 3-symbols group separation

--- js/app.js
+++ js/app.js
@@ -27,12 +27,13 @@
         $scope.prices.splice(index, 1);
       };
 
-      $scope.setEditStatus = function(index){
-        $scope.prices[index].status = 'editing';
-      };
-
       $scope.setOkStatus = function(index){
         $scope.prices[index].status = 'ok';
+      };
+      $scope.setEditingStatus = function(index){
+        if($scope.prices[index].status === 'ok'){
+          $scope.prices[index].status = 'editing';
+        }
       };
 
 
@@ -40,17 +41,61 @@
         var total = 0;
         angular.forEach($scope.prices, function(price) {
           console.log('price is:', price);
-          total += parseFloat(price.price) || 0;
+          total += parseInt(price.price, 10) || 0;
         });
         return total;
       };
 
       $scope.separatePrice = function(price){
-        var separated = price;
+        var priceToParse;
+        if(price){
+          priceToParse = price.toString();
+        } else {
+          priceToParse = '0';
+        }
+        var separated = priceToParse.replace(/\D/g, '');
+        var inputValue = separated
+          .replace(' ', '')
+          .split("")
+          .reverse()
+          .join("");
+        var newValue = '';
+        for (var i = 0; i < inputValue.length; i++) {
+          if (i % 3 === 0) {
+            newValue += ' ';
+          }
+          newValue += inputValue[i];
+        }
+        separated = newValue
+          .split("")
+          .reverse()
+          .join("")
+          .replace(/\s$/g,'');
 
+        if (price < 0){
+         separated ='-' + separated;
+        }
         return separated;
       };
-    }]);
+    }])
+
+    .directive('appType', function () {
+      return {
+        require: 'ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+          // Custom number validation logic.
+          if (attrs.appType === 'number') {
+            return ctrl.$parsers.push(function (value) {
+              var valid = value === null || isFinite(value);
+
+              ctrl.$setValidity('number', valid);
+
+              return valid && value !== null ? Number(value) : undefined;
+            });
+          }
+        }
+      };
+    });
 
 
 }());
